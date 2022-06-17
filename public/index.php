24763444fe5f468ad583adcfc26afa037afde8ec
ashkanfekridev <ashkanfekridev@gmail.com>
2022-06-17 14:33:40 +0430

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,12 @@
 
 // TODO
 // 1.include autoload files
+require_once __DIR__.'/../vendor/autoload.php';
 // 2.new instance form App class
+/**
+ * @var $app \PhpMvc\Foundation\Application;
+ */
+$app = \PhpMvc\Foundation\Application::getInstance();
 // 3.run router
 // 4.set routes
 // 4.resolve router
