5394335d6fcab2ad046f280195f6b8d8c3c13320
ashkanfekridev <ashkanfekridev@gmail.com>
2022-06-17 14:34:35 +0430

    create RouteServiceProvider.php

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Providers;
+use PhpMvc\Support\ServiceProvider;
+
+
+class RouteServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+    }
+}
