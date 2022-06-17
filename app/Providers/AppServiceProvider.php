82b6b19bf313de3801fe56d9c140d23070da0393
ashkanfekridev <ashkanfekridev@gmail.com>
2022-06-17 14:34:22 +0430

    create AppServiceProvider.php

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Providers;
+
+
+use PhpMvc\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+    }
+}
